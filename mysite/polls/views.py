cf5329f4273b6b780112a2a2247034c32887f29b
Lakshya Taragi <lakshya.taragi01@gmail.com>
2020-11-26 18:00:01 +0530

    Vote and view results views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
 # for render() method
 from django.shortcuts import render, get_object_or_404
 
+from django.urls import reverse
+
 # for raising 404errors
 from django.http import Http404
 
-from .models import Question
+from .models import Question, Choice 
 
 
 
@@ -46,9 +48,26 @@ def detail(request, question_id):
 
 
 
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id )
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id )
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
+
+
+# def vote(request, question_id):
+#     return HttpResponse("You are voting on question %s." % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request,'polls/detail.html', {'question':question,
+                                                    'error_message': "You didn't select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))                                               
